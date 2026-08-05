aaf70a46af3734969294ccb5da606607847282e2
Roman <ramongavrilov@gmail.com>
2026-08-05 22:56:32 +0300

    build(docker): настроил миграции и изменил нейминг контейнера с бекендом

--- back/infrastructure/migrations/001_create_tables.sql
+++ back/infrastructure/migrations/001_create_tables.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS customers (
 CREATE TABLE IF NOT EXISTS categories (
     category_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
-    parent_id UUID REFERENCES categories(category_id) ON DELETE SET NULL
+    parent_id UUID REFERENCES categories(category_id) ON DELETE SET NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Таблица товаров (объявлений)
@@ -31,7 +32,8 @@ CREATE TABLE IF NOT EXISTS products (
 CREATE TABLE IF NOT EXISTS wishlists (
     wishlist_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     product_id UUID NOT NULL UNIQUE REFERENCES products(product_id) ON DELETE CASCADE,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Таблица связей категорий с вишлистами (многие ко многим)
@@ -66,15 +68,15 @@ CREATE TABLE IF NOT EXISTS reviews (
 );
 
 -- Индексы для оптимизации запросов
-CREATE INDEX idx_products_customer_id ON products(customer_id);
-CREATE INDEX idx_products_category_id ON products(category_id);
-CREATE INDEX idx_products_is_active ON products(is_active);
-CREATE INDEX idx_wishlists_product_id ON wishlists(product_id);
-CREATE INDEX idx_chains_from_product_id ON chains(from_product_id);
-CREATE INDEX idx_chains_to_product_id ON chains(to_product_id);
-CREATE INDEX idx_chains_status ON chains(status);
-CREATE INDEX idx_reviews_to_customer_id ON reviews(to_customer_id);
-CREATE INDEX idx_reviews_from_customer_id ON reviews(from_customer_id);
+CREATE INDEX IF NOT EXISTS idx_products_customer_id ON products(customer_id);
+CREATE INDEX IF NOT EXISTS  idx_products_category_id ON products(category_id);
+CREATE INDEX IF NOT EXISTS  idx_products_is_active ON products(is_active);
+CREATE INDEX IF NOT EXISTS  idx_wishlists_product_id ON wishlists(product_id);
+CREATE INDEX IF NOT EXISTS  idx_chains_from_product_id ON chains(from_product_id);
+CREATE INDEX IF NOT EXISTS  idx_chains_to_product_id ON chains(to_product_id);
+CREATE INDEX IF NOT EXISTS  idx_chains_status ON chains(status);
+CREATE INDEX IF NOT EXISTS  idx_reviews_to_customer_id ON reviews(to_customer_id);
+CREATE INDEX IF NOT EXISTS  idx_reviews_from_customer_id ON reviews(from_customer_id);
 
 -- Триггер для обновления updated_at
 CREATE OR REPLACE FUNCTION update_updated_at_column()
@@ -85,9 +87,9 @@ BEGIN
 END;
 $$ language 'plpgsql';
 
-CREATE TRIGGER update_customers_updated_at BEFORE UPDATE ON customers FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-CREATE TRIGGER update_products_updated_at BEFORE UPDATE ON products FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-CREATE TRIGGER update_categories_updated_at BEFORE UPDATE ON categories FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-CREATE TRIGGER update_wishlists_updated_at BEFORE UPDATE ON wishlists FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-CREATE TRIGGER update_chains_updated_at BEFORE UPDATE ON chains FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
-CREATE TRIGGER update_reviews_updated_at BEFORE UPDATE ON reviews FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+CREATE OR REPLACE TRIGGER update_customers_updated_at BEFORE UPDATE ON customers FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+CREATE OR REPLACE TRIGGER update_products_updated_at BEFORE UPDATE ON products FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+CREATE OR REPLACE TRIGGER update_categories_updated_at BEFORE UPDATE ON categories FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+CREATE OR REPLACE TRIGGER update_wishlists_updated_at BEFORE UPDATE ON wishlists FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+CREATE OR REPLACE TRIGGER update_chains_updated_at BEFORE UPDATE ON chains FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
+CREATE OR REPLACE TRIGGER update_reviews_updated_at BEFORE UPDATE ON reviews FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
